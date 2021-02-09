95d1aba08717b79ce0e34457bd747884951f7a86
danaalrajeh <danaalrajeh@hotmail.com>
2021-02-09 19:46:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myassignment/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editTextTextPersonName2);
+        button =(Button) findViewById(R.id.button2);
+        textView = (TextView)findViewById(R.id.textView2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =editText.getText().toString();
+                textView.setText("Hello "+name);
+
+            }
+        });
+    }
+}
